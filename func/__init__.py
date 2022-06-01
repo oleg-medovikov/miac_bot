1b3a1c1fa799f84d40a596d21ef7cc946c063c96
oleg-medovikov <oleg.medovikov@mail.ru>
2022-06-01 15:07:55 +0300

    add 28 covid

--- func/__init__.py
+++ func/__init__.py
@@ -11,6 +11,7 @@ from .parus.svod_26_covid_19       import svod_26_covid_19
 from .parus.svod_27_covid_19       import svod_27_covid_19
 from .parus.svod_27_covid_19_regiz import svod_27_covid_19_regiz
 from .parus.svod_27_covid_small    import svod_27_covid_small
+from .parus.svod_28_covid_19       import svod_28_covid_19
 from .parus.svod_29_covid_19       import svod_29_covid_19
 from .parus.svod_33_covid_19       import svod_33_covid_19
 from .parus.svod_36_covid_19       import svod_36_covid_19
@@ -33,3 +34,5 @@ from .zam_mz.delete_zam_mz   import delete_zam_mz
 
 from .loader.report_guber      import report_guber
 from .loader.report_vp_and_cv  import report_vp_and_cv
+
+
