0c1923fa5bc2dda4c4d31b7ff6e855d51dcf4e15
oleg-medovikov <oleg.medovikov@mail.ru>
2022-06-01 14:38:12 +0300

    add 27 covid small

--- base/__init__.py
+++ base/__init__.py
@@ -5,6 +5,8 @@ from .parus import parus_sql
 from .covid import covid_sql, covid_exec, covid_insert
 # Работа с NsiBase
 from .nsibase import nsi_sql
+# Работа с MIAC_ds
+from .miac_ds import miac_ds_sql, miac_ds_insert
 
 
 # Таблицы обеспечивающие работу бота
--- func/__init__.py
+++ func/__init__.py
@@ -10,6 +10,7 @@ from .admin.check_robot            import check_robot
 from .parus.svod_26_covid_19       import svod_26_covid_19
 from .parus.svod_27_covid_19       import svod_27_covid_19
 from .parus.svod_27_covid_19_regiz import svod_27_covid_19_regiz
+from .parus.svod_27_covid_small    import svod_27_covid_small
 from .parus.svod_29_covid_19       import svod_29_covid_19
 from .parus.svod_33_covid_19       import svod_33_covid_19
 from .parus.svod_36_covid_19       import svod_36_covid_19
