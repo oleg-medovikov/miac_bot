c387a6466cb49176372d2a7cc51b8075a01f801d
oleg-medovikov <oleg.medovikov@mail.ru>
2022-06-28 16:46:45 +0300

    add task load_fr_death

--- shed/scheduler.py
+++ shed/scheduler.py
@@ -6,27 +6,11 @@ from conf import MASTER
 
 async def scheduler():
     aioschedule.every().day.at('01:00').do( load_fr )
-    aioschedule.every().day.at('15:35').do(cheak_robot)
+    aioschedule.every().day.at('02:00').do( load_fr )
     #aioschedule.every(1).minutes.do(test_send)
     while True:
         await aioschedule.run_pending()
         await asyncio.sleep(1)
-
-
-async def cheak_robot():
-    TASK = Task(
-        time_create = datetime.datetime.now(),
-        client = MASTER,
-        task_type = 'Sheduler',
-        c_id = 1,
-        c_func = 'check_robot',
-        c_arg = 'no',
-        users_list = str(MASTER),
-        time_start = None,
-        time_stop = None,
-        comment = None
-        )
-    TASK.add()
 
 async def load_fr():
     TASK = Task(
@@ -35,6 +19,21 @@ async def load_fr():
         task_type = 'Sheduler',
         c_id = 41,
         c_func = 'load_fr',
+        c_arg = 'no',
+        users_list = str(MASTER),
+        time_start = None,
+        time_stop = None,
+        comment = None
+        )
+    TASK.add()
+
+async def load_fr_death():
+    TASK = Task(
+        time_create = datetime.datetime.now(),
+        client = MASTER,
+        task_type = 'Sheduler',
+        c_id = 42,
+        c_func = 'load_fr_death',
         c_arg = 'no',
         users_list = str(MASTER),
         time_start = None,
