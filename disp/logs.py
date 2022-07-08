f348d9b7089b2e21d9b3a7449b5165686cabd567
oleg-medovikov <oleg.medovikov@mail.ru>
2022-07-08 17:25:23 +0300

    08.07.2022

--- disp/logs.py
+++ disp/logs.py
@@ -18,25 +18,26 @@ async def ask_log(message: types.Message):
 
     df = df.loc[~df['c_name'].isin(IGNORE) ].head( COUNT )
    
+    df['fio'] = df['fio'].str.split(' ',n=0,expand=True)
     df['time_create'] = pd.to_datetime(df['time_create']).dt.strftime('%H:%M')
     
     df['delta'] = (pd.to_datetime(df['time_stop']) \
             - pd.to_datetime(df['time_start'])).dt.total_seconds().round(decimals=1)
     
-    mess = f'```Последние {COUNT} выполненных задач:'
+    
+    mess = f'``` Последние {COUNT} выполненных задач:'
     mess += '\n' + '='*len(mess)
 
     _1 = df['fio'].str.len().max() 
     _2 = df['c_name'].str.len().max()
 
-
     for task in df.to_dict('records'):
         
         mess += '\n' + task['time_create'] +  ' | ' \
                 + task['fio']    + ' '*(_1 - len(task['fio']) )     +  ' | ' \
-                + task['c_name'] + ' '*(_2 - len(task['c_name']) )  +  ' | ' \
-                + str(task['delta']) + ' сек.'
-    
+                + task['c_name'][:13] + ' '*(13 - len(task['c_name'][:13]) ) + ' | ' \
+                + str(task['delta']) + ' с.'
+                #+ task['c_name'][:13] + ' '*(_2 - len(task['c_name']) )  +  ' | ' \
 
     mess += '```' 
 
