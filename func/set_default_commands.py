31f3fb2c20c6b828f0e082b2776453bc3a520a09
oleg-medovikov <oleg.medovikov@mail.ru>
2023-01-16 11:06:10 +0300

    change bot commands func

--- func/set_default_commands.py
+++ func/set_default_commands.py
@@ -1,19 +1,21 @@
 from aiogram.types import BotCommand
 
+
 async def set_default_commands(dp):
-    commands = [
-        BotCommand(command="start",
-            description="Приветсвие"),
-        BotCommand(command="tasks",
-            description="Получить файл с заданиями"),
-        BotCommand(command="users",
-            description="Получить файл с пользователями"),
-         BotCommand(command="commands",
-            description="Получить файл с командами"),
-         BotCommand(command="dirs",
-            description="Получить файл с директориями"),
-         BotCommand(command="access",
-            description="Получить файл с правами"),
-           ]
+    DICT = {
+        "start": "Приветствие",
+        "tasks": "Получить файл с заданиями",
+        "users": "Получить файл с пользователями",
+        "commands": "Получить файл с командами",
+        "dirs": "Получить файл с директориями",
+        "access": "Получить файл с правами",
+            }
+
+    commands = []
+    for key, value in DICT.items():
+        commands.append(BotCommand(
+            command=key,
+            description=value
+            ))
 
     await dp.bot.set_my_commands(commands)
